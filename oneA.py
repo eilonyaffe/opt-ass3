cdbe629f5a3df625938010357a2c0a456ef52210
Eilon Yaffe <93070667+eilonyaffe@users.noreply.github.com>
2024-06-24 13:20:01 +0300

    Update oneA.py

--- oneA.py
+++ oneA.py
@@ -1,13 +1,10 @@
 import numpy as np
 
-
-def jacobi(A, b, sigma):
+def jacobi(A, b, sigma, maxIter):
     n = len(b)
     x = np.zeros(n)  # initial guess
     diagonal_el = np.diag(A)
-    flag = True
-    k = 1  # TODO delete later
-    while flag:
+    for _ in range(maxIter):
         x_new = np.zeros(n)
         for i in range(n):
             row_i = A[i, :].copy()
@@ -19,20 +16,15 @@ def jacobi(A, b, sigma):
                 x_new[i] = (1 / diagonal_el[i]) * (b[i] - desired_sum)
         if np.linalg.norm(np.dot(A, x_new) - b) / np.linalg.norm(b) < sigma or np.linalg.norm(
                 x_new - x) / np.linalg.norm(x_new) < sigma:
-            return x_new, k
+            return x_new
         else:
             x = x_new
-            k += 1
-    return -1
 
-
-def gauss_seidel(A, b, sigma):
+def gauss_seidel(A, b, sigma, maxIter):
     n = len(b)
     x = np.zeros(n)
     diagonal_el = np.diag(A)
-    flag = True
-    k = 1  # TODO delete later
-    while flag:
+    for _ in range(maxIter):
         x_new = np.zeros(n)
         for i in range(n):
             row_i = A[i, :]
@@ -45,53 +37,43 @@ def gauss_seidel(A, b, sigma):
                 x_new[i] = (1 / diagonal_el[i]) * (b[i] - desired_sum)
         if np.linalg.norm(np.dot(A, x_new) - b) / np.linalg.norm(b) < sigma or np.linalg.norm(
                 x_new - x) / np.linalg.norm(x_new) < sigma:
-            return x_new, k
+            return x_new
         else:
             x = x_new
-            k += 1
-    return -1
 
-
-def steepest_descent(A, b, sigma):
+def steepest_descent(A, b, sigma, maxIter):
     n = len(b)
     x = np.zeros(n)  # Initial guess
     r = b.copy()-np.dot(A,x)  # Initial residual
-    k = 0  # Iteration counter
-
-    while True:
+    for _ in range(maxIter):
         a_rk = np.dot(A, r)
         alpha = np.dot(r,r)/np.dot(r,a_rk)
         new_x = x + alpha*r
         r = r - alpha*a_rk
         if (np.linalg.norm(np.dot(A, new_x) - b) / np.linalg.norm(b)) < sigma or (np.linalg.norm(
                 new_x - x) / np.linalg.norm(new_x)) < sigma:
-            return new_x, k
+            return new_x
         else:
             x = new_x
-            k += 1
 
-def conjugate_gradient(A, b, sigma):
+def conjugate_gradient(A, b, sigma, maxIter):
     n = len(b)
     x = np.zeros(n)  # Initial guess
     r = b.copy()-np.dot(A,x)  # Initial residual
-    p = b.copy()-np.dot(A,x)  # Initial residual
-    k = 0  # Iteration counter
-
-    while True:
+    p = b.copy()-np.dot(A,x)
+    for _ in range(maxIter):
         a_pk = np.dot(A, p)
         alpha = np.dot(r,r)/np.dot(p,a_pk)
         new_x = x + alpha*p
         new_r = r - alpha*a_pk
         if (np.linalg.norm(np.dot(A, new_x) - b) / np.linalg.norm(b)) < sigma or (np.linalg.norm(
                 new_x - x) / np.linalg.norm(new_x)) < sigma:
-            return new_x, k
+            return new_x
         else:
             beta = np.dot(new_r,new_r)/np.dot(r,r)
             p = new_r + beta*p
             x = new_x
             r = new_r
-            k += 1
-
 
 
 A = np.array([[4, -1, 1],
@@ -105,12 +87,12 @@ A_spd = np.array([[2, -1, 0],
 
 
 # non SPD
-print(jacobi(A, b, sigma))
-print(gauss_seidel(A, b, sigma))
-print(steepest_descent(A_spd, b, sigma))
+print(jacobi(A, b, sigma,100))
+print(gauss_seidel(A, b, sigma,100))
+print(steepest_descent(A, b, sigma,100))
 
 # SPD
-print(jacobi(A_spd, b, sigma))
-print(gauss_seidel(A_spd, b, sigma))
-print(steepest_descent(A_spd, b, sigma))
-print(conjugate_gradient(A_spd, b, sigma))
+print(jacobi(A_spd, b, sigma,100))
+print(gauss_seidel(A_spd, b, sigma,100))
+print(steepest_descent(A_spd, b, sigma,100))
+print(conjugate_gradient(A_spd, b, sigma,100))
